99eb8ec93a8fe0d381181f7050e17facf040e1ee
Oleksandr Kramarenko <oleks.kramarenko@gmail.com>
2021-06-08 10:33:13 +0300

    all files

--- Assets/Scripts/MovingReels.cs
+++ Assets/Scripts/MovingReels.cs
@@ -2,32 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class MovingReels : MonoBehaviour
 {
-    [SerializeField] private GameObject symbol0;
-    [SerializeField] private GameObject symbol3;
     private RectTransform rectTransform;
     [SerializeField] private Ease ease;
     [Range(0,10)][SerializeField] private float animationTime;
-    [SerializeField] private int animationLoops;
-
 
     // Start is called before the first frame update
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
-        Tweener tweener = rectTransform.DOAnchorPosY(-2400, animationTime).
+        Tweener tweener = rectTransform.DOAnchorPosY(-3200, animationTime).
             SetEase(ease);
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (Transform child in transform)
+        
+        foreach (RectTransform child in rectTransform)
         {
             if (child.position.y <= 223) {
-                child.position += Vector3.up * 800.0f; 
+                child.position += Vector3.up * 800.0f;
+                child.GetComponent<Image>().sprite = Resources.Load<Sprite>("symbols/icon_7");
             };
         }
     }
