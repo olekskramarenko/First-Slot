3223a81cc32c4832950134f39e72bc474d9fa5cc
olekskramarenko <oleks.kramarenko@gmail.com>
2021-08-28 11:08:08 +0300

    bug with diagonal win line animation fixed

--- Assets/Scripts/GameLogic/WinLinesChecker.cs
+++ Assets/Scripts/GameLogic/WinLinesChecker.cs
@@ -137,7 +137,7 @@ public class WinLinesChecker : MonoBehaviour
             yield return new WaitUntil(() => !prizeAnimator.IsAnimPlaying | prizeAnimator.IsStopPushed);
             WinLineCheck(winLine);
         }
-        yield return new WaitUntil(() => !symbols[symbols.Length - 1].SymbolAnimation.isPlaying);
+        yield return new WaitUntil(() => !prizeAnimator.IsAnimPlaying);
         prizeAnimator.UpdatePrizeCounter();
         CheckScatters();
         if (OnStateChanged != null) OnStateChanged(ReelStates.ReadyForSpin);
